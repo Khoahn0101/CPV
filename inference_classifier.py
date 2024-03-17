a3d1be08b78c2473ae1e6e74278926bd676e5bdf
Khoahn0101 <144107996+Khoahn0101@users.noreply.github.com>
2024-03-17 11:05:23 +0700

    Update inference_classifier.py

--- inference_classifier.py
+++ inference_classifier.py
@@ -1,21 +1,28 @@
 import pickle
-
 import cv2
 import mediapipe as mp
 import numpy as np
 
-model_dict = pickle.load(open('./model.p', 'rb'))
+model_dict = pickle.load(open("E:\CPV\Dataset\model.p", 'rb'))
 model = model_dict['model']
 
-cap = cv2.VideoCapture(2)
+# Open the webcam
+cap = cv2.VideoCapture(0)
 
+if not cap.isOpened():
+    print("Error: Unable to open camera.")
+    exit()
+
+# Initialize MediaPipe Hands
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
-
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-labels_dict = {0: 'A', 1: 'B', 2: 'L'}
+labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J',
+            10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S',
+            19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'}
+
 while True:
 
     data_aux = []
@@ -32,9 +39,9 @@ while True:
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             mp_drawing.draw_landmarks(
-                frame,  # image to draw
-                hand_landmarks,  # model output
-                mp_hands.HAND_CONNECTIONS,  # hand connections
+                frame, 
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
                 mp_drawing_styles.get_default_hand_landmarks_style(),
                 mp_drawing_styles.get_default_hand_connections_style())
 
@@ -42,7 +49,6 @@ while True:
             for i in range(len(hand_landmarks.landmark)):
                 x = hand_landmarks.landmark[i].x
                 y = hand_landmarks.landmark[i].y
-
                 x_.append(x)
                 y_.append(y)
 
@@ -54,21 +60,17 @@ while True:
 
         x1 = int(min(x_) * W) - 10
         y1 = int(min(y_) * H) - 10
-
         x2 = int(max(x_) * W) - 10
         y2 = int(max(y_) * H) - 10
-
         prediction = model.predict([np.asarray(data_aux)])
-
         predicted_character = labels_dict[int(prediction[0])]
-
         cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 4)
-        cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3,
-                    cv2.LINE_AA)
-
+        cv2.putText(frame, predicted_character, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 0, 0), 3, cv2.LINE_AA)
     cv2.imshow('frame', frame)
-    cv2.waitKey(1)
 
-
+    # Check for the 'q' key to exit the loop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 cap.release()
 cv2.destroyAllWindows()
+
